4f16c1dba0e5428c69a64b92c476e245bd928b79
Shawn Huang <69904687+Mutton9558@users.noreply.github.com>
2024-12-26 22:36:55 +0800

    Better Dapo intial files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-content-home">
+        <h1>Better Dapo</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Menus.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function Menus() {
+  return (
+    <>
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-content-menus">
+        <h1>Menus</h1>
+      </div>
+    </>
+  );
+}
+
+export default Menus;
