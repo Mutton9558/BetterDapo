7628144255d05c9d1aaa4089bef1ad630805fedd
Shawn Huang <69904687+Mutton9558@users.noreply.github.com>
2025-01-07 15:07:02 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
-<<<<<<< HEAD
-=======
 import Advertisement from "./components/Advertisement";
 import DapoLogo from "./assets/DapoSahang.png";
->>>>>>> 9a61175 (Added advertisement component)
 
 function App() {
   return (
@@ -14,13 +11,10 @@ function App() {
       </div>
       <div className="main-content-home">
         <h1>Better Dapo</h1>
-<<<<<<< HEAD
-=======
         <Advertisement
           image={DapoLogo}
           captionText="Fresh home-cooked version of Dapo Sahang's Menu!"
         />
->>>>>>> 9a61175 (Added advertisement component)
       </div>
     </>
   );
